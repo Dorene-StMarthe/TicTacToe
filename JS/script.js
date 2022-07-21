09dc9f31c0ae9b511e6c7a22c5d1cb048e873278
Dorene07 <dstmarth@gmail.com>
2022-07-21 19:13:38 -0400

    created object for game pieces. Not working.

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,11 @@
-const game = (()  => {
-  const players=(name, piece, ai, turn) => {
-    return { name, piece, ai, turn};
+const currentPlayer = (piece) => {
+  this.piece = piece;
+  const getPiece = () => {
+    return piece;
   };
-let player1 = players('player 1', 'X', false, true);
-let player2 = players('player 2', 'O', false, false);})
 
-
-const winningCombos = [
-  [0,1,2],
-  [0,3,6]
-  [3,4,5],
-  [6,7,8],
-  [1,4,7],
-  [2,4,6],
-  [2,5,8],
-  [0,4,8]
-];
+}
+console.log(currentPlayer.getPiece(X))
+const gameBoard = (() => {
+  const board = ["" ,"" ,"" ,"" ,"" ,"" ,"" ,"" ,""];
+})
