141c4cb81792ab44262b2da0160ad683c312601e
Dorene07 <dstmarth@gmail.com>
2022-07-21 21:46:10 -0400

    added a few DOM elements

--- JS/script.js
+++ JS/script.js
@@ -23,26 +23,12 @@ const getSquare = (idx) => {
   if (idx> board.length) return;
   return board[idx];
 };
-
-
-return { squareSetUp, getSquare, playAgain };
-})();
-
 const playAgain = () => {
   for (let i = 0; i < board.length; i++){
     board[i] ="";
   }
 };
 
-
-//if any combo returns true, then x || o wins!
-const winCombo = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
+return { squareSetUp, getSquare, playAgain };
+})();
+console.log(squareSetUp (2,X));
