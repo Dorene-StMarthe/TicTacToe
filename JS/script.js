b8468322b825b50a58087916f4c31d59eb84eca5
Dorene07 <dstmarth@gmail.com>
2022-07-21 21:16:00 -0400

    winning combo added

--- JS/script.js
+++ JS/script.js
@@ -23,12 +23,26 @@ const getSquare = (idx) => {
   if (idx> board.length) return;
   return board[idx];
 };
+
+
+return { squareSetUp, getSquare, playAgain };
+})();
+
 const playAgain = () => {
   for (let i = 0; i < board.length; i++){
     board[i] ="";
   }
 };
 
-return { squareSetUp, getSquare, playAgain };
-})();
-console.log(squareSetUp (2,X));
+
+//if any combo returns true, then x || o wins!
+const winCombo = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
