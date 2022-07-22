d46de6256f9595714cc05cc53d28fee5198f8ab5
Dorene07 <dstmarth@gmail.com>
2022-07-22 13:13:24 -0400

    game flow

--- JS/script.js
+++ JS/script.js
@@ -16,5 +16,30 @@ const winCombo = [
 
 
 
-document.querySelectorAll('.square').forEach(square => square.addEventListener('click', squareResponse));
-document.querySelector('.btn-PlayAgain').addEventListener('click', playAgainResponse);
+document.querySelectorAll('.square').forEach(square => square.addEventListener('click', ());
+document.querySelector('.btn-PlayAgain').addEventListener('click', ());
+
+
+function squareChosen( chosenSquare, idx) {
+  gameBoard[idx] = currentPlayer;
+  chosenSquare.textContent = currentPlayer;
+}
+
+
+function gameResults(){
+  let winningGame = false; //initializes game response to winning the game as false until conditions are met. 
+  for (let i=0; i <= 7; i++) {//iterating through winning combos [0-7]
+    const gameWin = winCombo[i];
+        let playerSign1 = gameBoard[gameWin[0]];
+        let playerSign2 = gameBoard[gameWin[1]];  //arrays to represent player selected square choices.
+        let playerSign3 = gameBoard[gameWin[2]];
+        if (playerSign1 === "" || playerSign2 === "" || playerSign3 === "") { //player peices are not the same.
+            continue;
+        }
+        if (playerSign1 === playerSign2 && playerSign2 === playerSign3) { //if player game symbols ("X || O" ) are all equal to each other, player wins!
+            winningGame = true;   
+            break // end loop
+        }
+    }
+}
+console.log(gameResults[1]);
