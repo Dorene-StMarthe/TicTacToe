6bbc81ca462230a7b3b01a186aceca950968670c
Dorene07 <dstmarth@gmail.com>
2022-07-22 15:18:41 -0400

    DOM

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,6 @@
+//Variables
+const gameStatus = document.querySelector('.displayMessage');
+
 let gameBoard = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer = "X";
 
@@ -13,28 +16,21 @@ const winCombo = [
 ];
 
 
-gameStatus.textContent = playersTurn();
+
 document.querySelectorAll('.square').forEach(square => square.addEventListener('click', clickedSquareEvent));
-const gameStatus = document.querySelector('.displayMessage');
+document.querySelector('.btn-PlayAgain').addEventListener('click', playAgainOption);
 
-const winningDisplay = () => "Player" + currentPlayer + " Won!";
 
-//_handleCellClick
-function clickedSquareEvent(squareSelection) {
+function clickedSquareEvent(squareSelection) { //When a square is clicked execution
   const chosenSquare = squareSelection.target;
   const idx = parseInt(chosenSquare.getAttribute('box'));
-
   if (gameBoard[idx] !== "" || !gameActive) {
       return;
   }
-
   squareChosen(chosenSquare, idx);
   gameResults();
 }
-
-
-
-// _handleCellPlayed
+//handle Player change
 function squareChosen( chosenSquare, idx) {
   gameBoard[idx] = currentPlayer;
   chosenSquare.textContent = currentPlayer;
@@ -42,7 +38,7 @@ function squareChosen( chosenSquare, idx) {
 
 
 
-//_handleResultsValidation
+
 function gameResults(){
   let winningGame = false; //initializes game response to winning the game as false until conditions are met. 
   for (let i=0; i <= 7; i++) {//iterating through winning combos [0-7]
@@ -58,19 +54,50 @@ function gameResults(){
             break // end loop
         } }
   if (winningGame) {
-  gameStatus.textContent = winningDisplay();
+  gameStatus.textContent = winningDisplay();   //display winners message
   gameActive = false;
   return;
 }
 
-players();
+let gameTie= !gameBoard.includes("");
+    if (gameTie) {
+        gameStatus.textContent = tiedGameMessage();  //tied game
+        gameActive = false;
+        return;
+    }
+      players();
 }
-
-//_handlePlayerChange
+//handle player change
+//Ternary 
 function players() {
-  currentPlayer =currentPlayer === "x" ? "O" : "X";
+  currentPlayer = currentPlayer === "x" ? "O" : "X";  //players
     gameStatus.textContent = playersTurn();
 }
 
+/*
+//If else
+let otherPlayer;
+if (currentplayer === "x"){
+  otherPlayer= "O"}
+  else{
+    otherPlayer= "x"
+  }
+*/
+
 let gameActive = true;
 
+
+function playAgainOption() {
+  gameActive = true;
+  currentPlayer = "X";
+  gameBoard = ["", "", "", "", "", "", "", "", ""];
+  gameStatus.innerHTML = playersTurn();;
+  document.querySelectorAll('square').forEach(square => square.textContent = "");
+}
+
+
+
+const winningDisplay = () => "Player" + currentPlayer + " Won!";
+const tiedGameMessage = () => "Tied Game!"
+const playersTurn = () => "It's" + currentPlayer + " turn!";
+gameStatus.textContent = playersTurn();
